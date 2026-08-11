b0396cfffbfc60741e3cd883ff37fa99aa532129
MAldeep <m7mdeldeeeep@gmail.com>
2026-08-11 17:40:32 +0300

    zod validations for auth and appointments

---
+++ src/validations/appointment.schema.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+
+export const createAppointmentSchema = z.object({
+  body: z.object({
+    patientName: z
+      .string()
+      .min(2, "Patient name must be at least 2 characters"),
+    doctor: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid Doctor ID format"),
+    date: z
+      .string()
+      .datetime()
+      .refine(
+        (val) => {
+          const inputDate = new Date(val);
+          const now = new Date();
+
+          now.setHours(0, 0, 0, 0);
+
+          return inputDate >= now;
+        },
+        {
+          message: "Date can't be in the past",
+        },
+      ),
+    status: z.enum(["scheduled", "completed", "cancelled"]).optional(),
+  }),
+});
+
+export const updateAppointmentStatusSchema = z.object({
+  params: z.object({
+    id: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid Appointment ID format"),
+  }),
+  body: z.object({
+    status: z.enum(["scheduled", "completed", "cancelled"]),
+  }),
+});
+export const getOrDeleteAppointmentParamsSchema = z.object({
+  params: z.object({
+    id: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid Doctor ID"),
+  }),
+});
+export type CreateAppointmentInput = z.infer<
+  typeof createAppointmentSchema
+>["body"];
