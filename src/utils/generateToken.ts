fb59ab0e0032d634f731bcaf831691d068b63aa8
MAldeep <m7mdeldeeeep@gmail.com>
2026-08-11 16:49:23 +0300

    generate Token in utils, utils done

---
+++ src/utils/generateToken.ts
@@ -0,0 +1,32 @@
+import { env } from "../config/env.js";
+import { UserRole } from "../types/user.types.js";
+import jwt from "jsonwebtoken";
+interface TokenPayload {
+  id: string;
+  role: UserRole;
+}
+
+interface TokenResponse {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export const generateToken = (
+  userId: string,
+  role: UserRole,
+): TokenResponse => {
+  const jwtAccessSecret = env.JWT_ACCESS_SECRET;
+  const jwtRefreshSecret = env.JWT_REFRESH_SECRET;
+  if (!jwtAccessSecret || !jwtRefreshSecret) {
+    throw new Error("JWT secrets are not defined in environment variables");
+  }
+  const accessToken = jwt.sign(
+    { id: userId, role } as TokenPayload,
+    jwtAccessSecret,
+    { expiresIn: "15m" },
+  );
+  const refreshToken = jwt.sign({ id: userId }, jwtRefreshSecret, {
+    expiresIn: "7d",
+  });
+  return { accessToken, refreshToken };
+};
