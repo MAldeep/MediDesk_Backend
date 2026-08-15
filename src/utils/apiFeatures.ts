df3572a40f00bc31ed8db6599ebbd3261064be0e
MAldeep <m7mdeldeeeep@gmail.com>
2026-08-15 21:22:47 +0300

    APIFreatures class as a factory for advanced filtering and search

---
+++ src/utils/apiFeatures.ts
@@ -0,0 +1,60 @@
+import { Query } from "mongoose";
+
+export class APIFeatures<T> {
+  public query: Query<T[], T>;
+  private queryString: Record<string, any>;
+  constructor(query: Query<T[], T>, queryString: Record<string, any>) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+  filter(): this {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ["page", "sort", "limit", "fields", "search"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+  search(searchableFields: string[]): this {
+    if (this.queryString.search) {
+      const keyword = this.queryString.search;
+      const searchConditions = searchableFields.map((field) => ({
+        [field]: { $regex: keyword, $options: "i" },
+      }));
+
+      this.query = this.query.find({
+        $or: searchConditions,
+      } as any);
+    }
+    return this;
+  }
+  sort(): this {
+    if (this.queryString.sort) {
+      const sortBy = (this.queryString.sort as string).split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+    return this;
+  }
+  limitFields(): this {
+    if (this.queryString.fields) {
+      const fields = (this.queryString.fields as string).split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select("-__v");
+    }
+    return this;
+  }
+  paginate(): this {
+    const page = Math.max(1, parseInt(this.queryString.page, 10) || 1);
+    const limit = Math.max(1, parseInt(this.queryString.limit, 10) || 10);
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
